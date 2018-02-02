5e665958f360d56fc48fd496f88fd358a1abc266
emymorningstar <35381999+emymorningstar@users.noreply.github.com>
2018-02-01 20:00:01 -0500

    Add files via upload
    starting project. includes submit button poorly designed layout and a text input

---
+++ layout.java
@@ -0,0 +1,35 @@
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class layout {
+	
+	public layout(JFrame frame) {
+	frame.setLayout(null);
+	JButton b = new JButton("submit");
+	JTextField t=new JTextField("");
+	t.setBounds(480, 415, 100, 40);
+	b.setBounds(580, 415, 100, 40);
+	frame.add(b);
+	frame.add(t);
+	
+	b.addActionListener(new ActionListener() {
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+		System.out.println("click");
+		Scanner sc=new Scanner(t.getText());
+		System.out.println(sc.next());
+		}
+		});
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBounds(50, 50, 700, 500);
+	frame.setVisible(true);
+	
+}}
---
+++ main.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class main {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("catchy title");
+		layout l= new layout(frame);
+	}
+
+}
