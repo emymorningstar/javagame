1724cb4fa4ea748a365a3f8426d19f7f5744c56e
emymorningstar <emymorningstar@gmail.com>
2018-02-08 18:34:44 -0500

    draws text and background color

--- layout.java
+++ layout.java
@@ -4,35 +4,81 @@ import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Scanner;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
 
 public class layout {
+//based drawing class on a prevous assignment I did -emy
 	
-	public layout(JFrame frame) {
-	frame.setLayout(null);
-	JButton b = new JButton("submit");
-	JTextField t=new JTextField("");
-	t.setBounds(480, 415, 100, 40);
-	b.setBounds(580, 415, 100, 40);
-	frame.add(b);
-	frame.add(t);
-	
-	b.addActionListener(new ActionListener() {
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-		System.out.println("click");
-		Scanner sc=new Scanner(t.getText());
-		if(sc.hasNext()) {
-		String cmd=sc.next();
-		System.out.println(cmd);
-		t.setText("");}
-		}
-		});
+	JFrame frame;
+	Canvas canvas;
+	public static final int WIDTH = 700;
+	public static final int HEIGHT = 500;
+	BufferStrategy bufferStrategy;
+
+	public layout() {
+		frame = new JFrame("Adventure Path: A Trail of Adventure");
+		JButton b = new JButton("submit");
+		JTextField t=new JTextField("");
+		t.setBounds(480, 415, 100, 40);
+		b.setBounds(580, 415, 100, 40);
+		frame.add(b);
+		frame.add(t);
+		
+		b.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+			System.out.println("click");
+			Scanner sc=new Scanner(t.getText());
+			if(sc.hasNext()) {
+			String cmd=sc.next();
+			System.out.println(cmd);
+			t.setText("");}
+			}
+			});
+		JPanel panel = (JPanel) frame.getContentPane();
+		panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		panel.setLayout(null);
+		canvas = new Canvas();
+		canvas.setBounds(0, 0, WIDTH, HEIGHT);
+		//canvas.setIgnoreRepaint(true);
+		frame.pack();
+		frame.setSize(WIDTH + 6, HEIGHT + 28);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setBounds(50, 50, 700, 500);
-	frame.setVisible(true);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setVisible(true);
+		panel.add(canvas);
+		canvas.createBufferStrategy(2);
+		bufferStrategy = canvas.getBufferStrategy();
+		canvas.requestFocus();
+		canvas.setBackground(Color.blue);
+		
+	}
 	
-}}
+	void render() {
+		Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
+		g.clearRect(0, 0, WIDTH, HEIGHT);
+		render(g);
+		g.dispose();
+		bufferStrategy.show();
+	}
+	
+	void render(Graphics2D g) {
+		g.drawString("HELLO WORLD" , 610, 50);
+		
+	}
+}
--- main.java
+++ main.java
@@ -3,8 +3,18 @@ import javax.swing.JFrame;
 public class main {
 
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("catchy title");
-		layout l= new layout(frame);
-	}
+		layout l= new layout();
+		
+		while (true) {
+			l.render();
+			try {
+				Thread.sleep(10);
 
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+
+			}
+		}
+	}
+	
 }
