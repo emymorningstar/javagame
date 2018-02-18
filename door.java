7a80504c88ff16ca8fe6c4ec382ead62dae20a67
burgerbaron <31454265+burgerbaron@users.noreply.github.com>
2018-02-18 12:48:21 -0500

    Completely changed

--- door.java
+++ door.java
@@ -1,85 +1,19 @@
 
+package adventurePath;
 
 import java.util.Random;
-public class door 
-{
-	// assigns directions to numbers
-	public static final int UNDEFINED = 0;
-	public static final int NORTH = 1;
-	public static final int SOUTH = 2;
-	public static final int EAST  = 3;
-	public static final int WEST  = 4;
-	public static final int UP    = 5;
-	public static final int DOWN  = 6;
-	public static final int NORTHEAST = 7;
-	public static final int NORTHWEST = 8;
-	public static final int SOUTHEAST = 9;
-	public static final int SOUTHWEST = 10;
-	public static final int IN = 11;
-	public static final int OUT = 12;
+public class Door 
+{	
 
-	// String codes	
-	public static final String[] dirName = 
-	{ 
-		"UNDEFINED",
-		"NORTH",
-		"SOUTH",
-		"EAST",
-		"WEST",
-		"UP",
-		"DOWN",
-		"NORTHEAST",
-		"NORTHWEST",
-		"SOUTHEAST",
-		"SOUTHWEST",
-		"IN",
-		"OUT"
-	};
+	private Room leadsTo = null;
 
-	public static final String[] shortDirName = 
+	public Door()
 	{
-		"NULL",
-		"N",
-		"S",
-		"E",
-		"W",
-		"U",
-		"D",
-		"NE",
-		"NW",
-		"SE",
-		"SW",
-		"I",
-		"O"		
-	};
-
-	private room leadsTo = null;
-	private int direction;
-
-	// full direction names
-	private String directionName;
-
-	// short direction names
-	private String shortDirectionName;
-
-	public door()
-	{
-		direction = door.UNDEFINED;
 		leadsTo = null;
-		directionName = dirName[UNDEFINED];
-		shortDirectionName = shortDirName[UNDEFINED];
 	}
 
-	public door( int d, room to1, room to2, room to3 )
+	public Door(Room to1, Room to2, Room to3 )
 	{
-		direction = d;
-
-		// Assign direction names
-		if (direction <= dirName.length )
-			directionName = dirName[direction];
-		if (direction <= shortDirName.length )
-			shortDirectionName = shortDirName[direction];
-
 		Random r = new Random();
 		int n = r.nextInt(2)+1;
 		
@@ -94,39 +28,14 @@ public class door
 		}
 	}
 
-	public String toString()
-	{
-		return directionName;
-	}
-
-	public void setDirectionName( String dirname )
-	{
-		directionName = dirname;
-	}
-
-	public String getDirectionName()
-	{
-		return directionName;
-	}
-
-	public void setShortDirectionName ( String shortName )
-	{
-		shortDirectionName = shortName;
-	}
-
-	public String getShortDirectionName ()
-	{
-		return shortDirectionName;
-	}
-
-	public void setLeadsTo ( room to )
+	public void setLeadsTo ( Room to )
 	{
 		leadsTo = to;
 	}
 
-	public room getLeadsTo (  )
+	public Room getLeadsTo (  )
 	{
 		return leadsTo;
 	}
-
+	
 }
