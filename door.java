12b21974678f327b5f78eb32e350e518b380c455
emymorningstar <emymorningstar@gmail.com>
2018-02-28 16:16:53 -0500

    add javadoc comments to door

--- door.java
+++ door.java
@@ -4,12 +4,20 @@ public class Door
 {	
 
 	private Room leadsTo = null;
-
+	/**
+	 *default room constructor
+	 * 
+	 */
 	public Door()
 	{
 		leadsTo = null;
 	}
-
+	/**
+	 *room constructor sets leads to possibilities
+	 *
+	 *@param 		Room to1, Room to2,Room to3
+	 * 
+	 */
 	public Door(Room to1, Room to2, Room to3 )
 	{
 		Random r = new Random();
@@ -25,12 +33,22 @@ public class Door
 			leadsTo = to3;
 		}
 	}
-
+	/**
+	 *sets leads to for a room
+	 *
+	 *@param 		Room to
+	 * 
+	 */
 	public void setLeadsTo ( Room to )
 	{
 		leadsTo = to;
 	}
-
+	/**
+	 *gets leads to for a room
+	 *
+	 *@returns 		Room leadsto
+	 * 
+	 */
 	public Room getLeadsTo (  )
 	{
 		return leadsTo;
