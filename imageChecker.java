28b0c0ae470a6539e935f1164c75cc14d7617096
emymorningstar <emymorningstar@gmail.com>
2018-04-06 16:58:12 -0400

    new backgrounds

--- imageChecker.java
+++ imageChecker.java
@@ -12,11 +12,31 @@ public class imageChecker {
 							ImageIO.read(new File("res/dragonroom.png")));
 				} catch (IOException e) {
 				}
+			} else if (instances.getCurrentRoom().getTitle() == "Fairy Forest") {
+				try {
+					instances.setCurrentImage(
+							ImageIO.read(new File("res/fairyroom.png")));
+				} catch (IOException e) {
+				}}
+			else if (instances.getCurrentRoom().getTitle() == "Secret Cave") {
+				try {
+					instances.setCurrentImage(
+							ImageIO.read(new File("res/secretcaveroom.png")));
+				} catch (IOException e) {
+				}
 			}
-			try {
-				instances.setCurrentImage(
-						ImageIO.read(new File("res/villageroom.png")));
-			} catch (IOException e) {
+			else if (instances.getCurrentRoom().getTitle() == "Work") {
+				try {
+					instances.setCurrentImage(
+							ImageIO.read(new File("res/workroom.png")));
+				} catch (IOException e) {
+				}
+			} else {
+				try {
+					instances.setCurrentImage(
+							ImageIO.read(new File("res/villageroom.png")));
+				} catch (IOException e) {
+				}
 			}
 		}
 	}
